1bd5739d81fce016dae102f8a4674a05605aaa2a
hafezmehrjoo <hafezmehrjoo@gmail.com>
2020-04-03 15:50:31 +0430

    Implementation SplitData for split x ,y model_data_generator

---
+++ CartPole/SplitData.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def split_data(training_data):
+    X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]))
+    y = np.array([i[1] for i in training_data]).reshape(-1, len(training_data[0][1]))
+    return X, y
